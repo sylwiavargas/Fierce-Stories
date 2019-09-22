0dfacaa8ef4db557045484354939ff8b2a7a5814
sylwiavargas <sylwia.vargas@gmail.com>
2019-09-22 15:45:01 -0400

    fonts

--- config/application.rb
+++ config/application.rb
@@ -6,14 +6,15 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module FierceStories
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
     config.serve_static_assets = true
     config.assets.initialize_on_precompile = false
     config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
