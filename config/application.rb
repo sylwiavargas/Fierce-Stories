47643daed5dd50d57a8c4f57844cf48592f73670
sylwiavargas <sylwia.vargas@gmail.com>
2019-09-22 15:20:00 -0400

    fonts

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module FierceStories
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.serve_static_assets = true
+    # config.serve_static_assets = true
     config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
